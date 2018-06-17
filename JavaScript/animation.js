f388433f7c88008be76a44ab4e48f09b58872958
Parasto Saharkhiz <psaha091@uottawa.ca>
2018-06-17 02:50:12 -0400

    Modified animation, not working yet

--- JavaScript/animation.js
+++ JavaScript/animation.js
@@ -1,14 +1,47 @@
 //$.getScript("https://unpkg.com/scrollreveal/dist/scrollreveal.min.js", function() {
-//  
-//    
+$.getScript("../assets/scrollreveal-master/dist/scrollreveal.min.js", function() {
+    
+    var config = {
+          viewFactor : 0.15,
+          duration   : 800,
+          distance   : "0px",
+          scale      : 0.8,
+        }
+
+    window.sr = new ScrollReveal(config)
+    
 //    window.sr = ScrollReveal();
-//    sr.reveal('.popularShirt',{ duration: 2000, delay: 500 }, 50);
+//    sr.reveal('.shirt');
+    
 ////    duration: 500;
 ////    delay: 10;
 //    sr.reveal('.reviews',{ duration: 2000, delay: 500 }, 50);
-//    
-//    
-//});
+    
+    var crown = document.querySelector(".popularShirt")
+      var demo  = document.querySelector(".shirt")
+
+      window.addEventListener( "scroll", scrollBrain, false )
+
+      var body = document.body,
+          html = document.documentElement;
+
+      var height = Math.max( body.scrollHeight, body.offsetHeight,
+                             html.clientHeight, html.scrollHeight, html.offsetHeight )
+
+      function scrollBrain(){
+
+        if ( window.pageYOffset >= demo.offsetTop + 64 ){
+          return crown.classList.add("visible")
+        }
+        if ( crown.classList.contains("visible") ){
+          return crown.classList.remove("visible")
+        }
+        if ( window.pageYOffset >= 0.85 * ( height - window.clientHeight ) ) {
+          _gaq.push([ '_trackEvent', 'Behavior', 'Scroll', 'Viewed the entire page.'])
+        }
+      }
+    
+});
 
 
 $(document).ready(function () {
@@ -19,7 +52,7 @@ $(document).ready(function () {
     window.onscroll = function() {
       if (window.pageYOffset > 0) {
         var opac = (window.pageYOffset / content);
-        document.getElementsByClassName("bg")[0].style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(../Photos/graffiti_wall.jpg) no-repeat";
+        document.getElementsByClassName("bg")[0].style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(../Photos/city-street-background.jpg) no-repeat";
         document.getElementsByClassName("bg")[0].style.backgroundSize = "100%";
       }
     }
