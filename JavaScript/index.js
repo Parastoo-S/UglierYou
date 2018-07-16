4eab8c8dd535cf2075f902a017cd4b89237ce9e9
Parasto Saharkhiz <psaha091@uottawa.ca>
2018-07-15 23:54:39 -0400

    Added scroll fading effect of jumbotron

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,78 +1,52 @@
-//$.getScript("../assets/scrollreveal-master/dist/scrollreveal.min.js", function() {
-//    
-//    var config = {
-//          viewFactor : 0.15,
-//          duration   : 500,
-//          distance   : "0px",
-//          scale      : 0.8,
-//    }
-//    
-//    
-//
-//    window.sr = new ScrollReveal(config)
-//    window.sr = ScrollReveal({ reset: false });
-//    sr.reveal('.cool',{ duration: 1000, delay: 50}, 50);
-//    sr.reveal('.popular',{ duration: 1000, delay: 50}, 50);
-//    sr.reveal('.reviews',{ duration: 2000, delay: 50}, 50);
-//    
-//    
-//});
-// 
-//$(document).ready(function () {
-//    $('#main').fadeIn(1000).removeClass('hidden');
-//    
-//    
-//    var content = document.querySelector("div.content").offsetTop;
-//    
-//    window.onscroll = function() {
-//        if (window.pageYOffset > 0) {
-//            var opac = (window.pageYOffset / content);
-//            document.getElementsByClassName("bg")[0].style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(../Photos/BackgroundCompletelyErased2.png) no-repeat";
-//            document.getElementsByClassName("bg")[0].style.backgroundSize = "100%";
-//
-//        }
+$.getScript("../assets/scrollreveal-master/dist/scrollreveal.min.js", function() {
+    
+    var config = {
+          viewFactor : 0.15,
+          duration   : 500,
+          distance   : "0px",
+          scale      : 0.8,
+    }
+    
+    
+
+    window.sr = new ScrollReveal(config)
+    window.sr = ScrollReveal({ reset: false });
+    sr.reveal('.cool',{ duration: 1000, delay: 50}, 50);
+    sr.reveal('.popular',{ duration: 1000, delay: 50}, 50);
+    sr.reveal('.reviews',{ duration: 2000, delay: 50}, 50);
+    
+    
+});
+ 
+$(document).ready(function () {
+    $('#main').fadeIn(1000).removeClass('hidden');
+    
+    
+    var content = document.querySelector("div.section1").offsetTop;
+    
+    window.onscroll = function() {
+        if (window.pageYOffset > 0) {
+            var opac = (window.pageYOffset / content);
+            document.getElementsByClassName("section1-container")[0].style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(../Photos/DesignsAdded.png) no-repeat";
+            document.getElementsByClassName("section1-container")[0].style.backgroundSize = "100%";
+
+        }
+        
 //        var navbar = document.querySelector("nav.navbar").offsetTop;
 //        if ($(window).scrollTop() >= 50) {
-//            document.getElementsByClassName("navbar")[0].style.backgroundColor = "rgba(0,0,0,0.5)";
+//            document.getElementsByClassName("navbar")[0].style.backgroundColor = "rgba(0, 32, 64, 1)";
 //            } 
 //        else {
 //            document.getElementsByClassName("navbar")[0].style.backgroundColor ='transparent';
 //        }
-//          
-//    }
-//    
-//    
-//    
-//});
-//
-//
-//
-//
-//$(document).ready(function() {
-//    // Add smooth scrolling to all links in navbar + footer link
-//    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-//
-//        // Make sure this.hash has a value before overriding default behavior
-//        if (this.hash !== "") {
-//
-//            // Prevent default anchor click behavior
-//            event.preventDefault();
-//
-//            // Store hash
-//            var hash = this.hash;
-//
-//            // Using jQuery's animate() method to add smooth page scroll
-//            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-//            $('html, body').animate({
-//                scrollTop: $(hash).offset().top
-//            }, 900, function() {
-//
-//                // Add hash (#) to URL when done scrolling (default click behavior)
-//                window.location.hash = hash;
-//            });
-//        } // End if
-//    });
-//})
+          
+    }
+    
+    
+    
+});
+
+
 
 $.getScript("../assets/fabric.min.js", function() {
     var canvas = this.__canvas = new fabric.Canvas('c');
